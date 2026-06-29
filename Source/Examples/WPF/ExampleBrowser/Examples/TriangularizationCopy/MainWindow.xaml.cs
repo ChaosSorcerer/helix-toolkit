53173a05a8b44e2052e47b841dcfa09a6ba258d8
m.waanders <37366622+ChaosSorcerer@users.noreply.github.com>
2026-06-29 17:49:44 +0200

    Added a temporary example to the package to illustrate the current shading issue. See "Shading issue" in the example browser. (note: whilst a shading issue reproduces it has some visual artifacts in the mesh that are not apparent in the original. Might need to double check this for a bit, perhaps a copy paste error in the triangle indices  :/

---
+++ Source/Examples/WPF/ExampleBrowser/Examples/TriangularizationCopy/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace ExampleBrowser.Examples.TriangularizationCopy;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+[ExampleBrowser.Example("Shading issue", "Illustrated a regression in polygon shading (re-uses a part of the triangularization demo for this).")]
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
